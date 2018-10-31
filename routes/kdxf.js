97f9cd716ac707e661dc42107aeb9bd14ffc920d
zoofox <457033517@qq.com>
2018-11-01 00:45:35 +0800

    kdxf

--- routes/kdxf.js
+++ routes/kdxf.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var fs = require("fs");
 var kdxf = require('../utils/kdxf');
 var path = require('path');
+var querystring = require("querystring");
 
 router.get('/text2audio', function(req, res, next) {
 	var name = req.query.name;
@@ -11,6 +12,14 @@ router.get('/text2audio', function(req, res, next) {
 	kdxf.text2audio(text, name, id, function(ret) {
 		res.send(ret)
 	});
+});
+router.post('/text2audio', function(req, res, next) {
+	var textarr = eval(req.body.textarr);
+	var id = req.body.id; //用于新建目录，各用户单独一个文件夹
+	
+	// kdxf.text2audio(text, name, id, function(ret) {
+		res.send({code:0,urls:[]});
+	// });
 });
 
 router.get('/clearfiles', function(req, res, next) {
